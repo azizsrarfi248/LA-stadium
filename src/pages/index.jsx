ff10a826e8920c6757cafd40e25de1d477056b11
aziz srarfi <azizsrarfi79@gmail.com>
2023-04-02 23:58:09 +0100

    setting up daisyui

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,11 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1 className="text-red-700  text-6xl">hello from the other side</h1>
-        <button>khalil</button>
-        <input type="text" />
-      </main>
+      <main>home</main>
     </>
   );
 }

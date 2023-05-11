9fdcae4fc5890ce4bfe0d7970a32cbd0cf4bb473
loueyhds <loueyhdoussa@gmail.com>
2023-05-11 13:43:37 +0200

    add photo to the modal

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -41,11 +41,7 @@ export default function Home() {
                 <h1 className="text-4xl font-extrabold">test</h1>
                 <div className="card w-96 bg-base-100 shadow-xl">
                   <figure className="px-10 pt-10">
-                    <img
-                      src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-                      alt="Shoes"
-                      className="rounded-xl"
-                    />
+                    <img src="/images.jpg" alt="Shoes" className="rounded-xl" />
                   </figure>
                   <div className="card-body items-center text-center ">
                     <h2 className="card-title">Shoes!</h2>

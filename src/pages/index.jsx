c897d5b97427efea069b9ae7ec709cd73c064368
loueyhds <loueyhdoussa@gmail.com>
2023-05-11 14:17:26 +0200

    add card to the modal

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -39,19 +39,40 @@ export default function Home() {
                 setIsOpen={setIsOpen}
               >
                 <h1 className="text-4xl font-extrabold">test</h1>
-                <div className="card w-96 bg-base-100 shadow-xl">
-                  <figure className="px-10 pt-10">
-                    <img src="/images.jpg" alt="Shoes" className="rounded-xl" />
-                  </figure>
-                  <div className="card-body items-center text-center ">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions">
-                      <button className="btn btn-primary">choose</button>
+                <div className="grid grid-cols-1 lg:grid-cols-2">
+                  <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                      <img
+                        src="/images.jpg"
+                        alt="Shoes"
+                        className="rounded-xl"
+                      />
+                    </figure>
+                    <div className="card-body items-center text-center ">
+                      <h2 className="card-title">Shoes!</h2>
+                      <p>If a dog chews shoes whose shoes does he choose?</p>
+                      <div className="card-actions">
+                        <button className="btn btn-primary">choose</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                      <img
+                        src="/images.jpg"
+                        alt="Shoes"
+                        className="rounded-xl"
+                      />
+                    </figure>
+                    <div className="card-body items-center text-center ">
+                      <h2 className="card-title">Shoes!</h2>
+                      <p>If a dog chews shoes whose shoes does he choose?</p>
+                      <div className="card-actions">
+                        <button className="btn btn-primary">choose</button>
+                      </div>
                     </div>
                   </div>
                 </div>
-
                 <button
                   className="btn btn-circle absolute bottom-6 bg-center"
                   onClick={() => setIsOpen(false)}
--- src/pages/training.jsx
+++ src/pages/training.jsx
@@ -1,5 +1,5 @@
 function Training() {
-  return <div>Training from louey</div>;
+  return <div></div>;
 }
 
 export default Training;
